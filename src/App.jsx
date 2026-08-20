0b2934ea05fafdb0048f62a127e0cb2542debf50
김태웅 <a01093831065@gmail.com>
2026-08-20 20:54:26 +0900

    최근 전적 화면 개발

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ export default function App() {
       <h1>quick.gg</h1>
       <input id="q" placeholder="소환사명 입력" />
       <button onClick={() => alert(document.getElementById('q').value + ' 검색')}>검색</button>
+
+      <div style={{ width: '250px', margin: '30px auto', padding: '15px', background: '#1a2635', color: '#fff', borderRadius: '10px' }}>
+        <h3>소환사명</h3>
+        <p>레벨: 100 / 다이아몬드</p>
+      </div>
     </div>
   )
 }
