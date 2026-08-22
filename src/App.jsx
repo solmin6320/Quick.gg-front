e5a7e43cdc327cbcdc24de5d893196b116a72513
김태웅 <a01093831065@gmail.com>
2026-08-22 20:05:27 +0900

    최근 전적 화면 개발

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ export default function App() {
         <h3>소환사명</h3>
         <p>레벨: 100 / 다이아몬드</p>
       </div>
+
+      <div style={{ width: '250px', margin: '30px auto', padding: '15px', background: '#1a2635', color: '#fff', borderRadius: '10px' }}>
+        <h3>최근 전적</h3>
+        <p>승리 - 킬 10 / 데스 2 / 어시 5</p>
+        <p>패배 - 킬 3 / 데스 7 / 어시 4</p>
+        <p>승리 - 킬 8 / 데스 1 / 어시 9</p>
+      </div>
     </div>
   )
 }
